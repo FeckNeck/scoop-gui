f284fa4b3cb2cac22c7adb6e363c9eace2c71b13
FeckNeck <math.dousse@hotmail.fr>
2023-11-01 10:01:33 +0100

    feat: update Scoop

--- apps/api/app/Controllers/Http/ScoopsController.ts
+++ apps/api/app/Controllers/Http/ScoopsController.ts
@@ -1,8 +1,6 @@
 import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
 import { getScoopStatus } from "../../modules/scrap";
 import execa from "execa";
-import fs from "fs";
-import Application from "@ioc:Adonis/Core/Application";
 
 export default class ScoopsController {
   public async status() {
@@ -11,9 +9,18 @@ export default class ScoopsController {
     return ScoopStatus;
   }
 
-  public async updateAll() {
-    const { stdout } = await execa("scoop", ["update", "*"]);
-    return stdout;
+  public async cache() {
+    const { stdout } = await execa("scoop", ["cache"]);
+    const cache = stdout.split("\n")[1];
+    return cache;
+  }
+
+  public async check() {
+    const { stdout } = await execa("scoop", ["-v"]);
+    if (stdout.includes("Current Scoop version:")) {
+      return true;
+    }
+    return false;
   }
 
   public async update({ params, response }: HttpContextContract) {
@@ -28,35 +35,20 @@ export default class ScoopsController {
     return stdout;
   }
 
-  public async cleanAll() {
-    const { stdout } = await execa("scoop", ["cleanup", "*"]);
-    return stdout;
-  }
-
   public async clean({ params }: HttpContextContract) {
     const { id } = params;
     const { stdout } = await execa("scoop", ["cleanup", id]);
     return stdout;
   }
 
-  public async check() {
-    const { stdout } = await execa("scoop", ["-v"]);
-    if (stdout.includes("Current Scoop version:")) {
-      return true;
-    }
-    return false;
-  }
-
-  public async import() {
-    const { stdout } = await execa("scoop", ["import"]);
+  public async import({ params }: HttpContextContract) {
+    const { path } = params;
+    const { stdout } = await execa("scoop", ["import", path]);
     return stdout;
   }
 
-  public async export({ request, response }: HttpContextContract) {
-    // const file = request.file("file");
-    // if (file) {
-    //   await file.move(Application.tmpPath("uploads"));
-    // }
-    return response.download(Application.tmpPath("uploads/eric_mange.jpg"));
+  public async export() {
+    const { stdout } = await execa("scoop", ["export"]);
+    return stdout;
   }
 }
--- apps/api/start/routes/scoop.ts
+++ apps/api/start/routes/scoop.ts
@@ -2,27 +2,14 @@ import Route from "@ioc:Adonis/Core/Route";
 
 Route.group(() => {
   Route.get("/status", "ScoopsController.status").as("status");
+  Route.get("/cache", "ScoopsController.cache").as("cache");
   Route.get("/check", "ScoopsController.check").as("check");
 
   Route.post("/import", "ScoopsController.import").as("import");
   Route.post("/export", "ScoopsController.export").as("export");
 
-  Route.post("/install/:id", "ScoopsController.install").as("install");
-  Route.delete("/uninstall/:id", "ScoopsController.uninstall").as("uninstall");
-
-  Route.group(() => {
-    Route.post("/", "ScoopsController.updateAll").as("updateAll");
-    Route.put("/:id", "ScoopsController.update").as("update");
-  })
-    .prefix("/update")
-    .as("update");
-
-  Route.group(() => {
-    Route.post("/", "ScoopsController.cleanAll").as("cleanAll");
-    Route.put("/:id", "ScoopsController.clean").as("clean");
-  })
-    .prefix("/clean")
-    .as("clean");
+  Route.post("/update/:id", "ScoopsController.update").as("update");
+  Route.post("/clean/:id", "ScoopsController.clean").as("clean");
 })
   .prefix("/scoop")
   .as("scoop");
