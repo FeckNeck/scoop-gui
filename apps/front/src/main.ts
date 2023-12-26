37092d7360738aa844c1edc2981d337922e75224
feckneck <math.dousse@hotmail.fr>
2023-12-26 18:07:10 +0100

    feat: remove vuetify

--- apps/front/src/main.ts
+++ apps/front/src/main.ts
@@ -2,15 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./styles.css";
 import { VueQueryPlugin, VueQueryPluginOptions } from "@tanstack/vue-query";
-import { createVuetify } from "vuetify";
-import { VSkeletonLoader } from "vuetify/labs/VSkeletonLoader";
-
 const app = createApp(App);
-// const vuetify = createVuetify({
-//   components: {
-//     VSkeletonLoader,
-//   },
-// });
 
 const vueQueryPluginOptions: VueQueryPluginOptions = {
   queryClientConfig: {
@@ -23,5 +15,4 @@ const vueQueryPluginOptions: VueQueryPluginOptions = {
 };
 
 app.use(VueQueryPlugin, vueQueryPluginOptions);
-// app.use(vuetify);
 app.mount("#app");
